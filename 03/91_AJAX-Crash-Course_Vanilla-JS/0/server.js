c64d36d0a83eee2bf6509efc536aba8db5b1511b
JoshuaHolloway <jhollow6@asu.edu>
2020-02-06 18:19:54 -0600

    removed

--- 03/91_AJAX-Crash-Course_Vanilla-JS/0/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-const port_num = 3000;
-app.listen(port_num, () => {
-    console.log('http://localhost:3000');
-});
