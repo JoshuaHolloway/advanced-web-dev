4d90ffa816f4ca287e14405390f19bef9971d54f
JoshuaHolloway <jhollow6@asu.edu>
2020-03-11 19:52:20 -0500

    returning promise from fetch api

--- 03/700_APIs/00_Pokemon-API/main.js
+++ 03/700_APIs/00_Pokemon-API/main.js
@@ -4,11 +4,29 @@ const apiData = {
     id: '25',
 };
 
-// Deconstruction
+// Destructuring
 const {url, type, id} = apiData;
+// Same as:
+// const url = apiData.url;
+// const type = apiData.type;
+// const id = apiData.id;
 
 // Build URL to get data
 // const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}`;
-const apiUrl = `${url}${type}/${id}`;
+const apiUrl = `${url}${type}/${id}`; // Template litteral
+// Object Literals: {}
+// Boolean Literals: true, false
+// String Literals: '', ""
+// Template Literals: ``
 
 console.log(apiUrl);
+
+// fetch API returns a promise
+fetch(apiUrl)
+    .then( (data) => {
+        // return in JSON format
+        return data.json();
+    } )
+    .then( (pokemon) => {
+        return console.log(pokemon);
+    });
