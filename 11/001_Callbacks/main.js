0e8970c61b6e01981d890d8746aab97da7d1e335
JoshuaHolloway <jhollow6@asu.edu>
2020-03-18 11:25:21 -0500

    changing color of background using index of element in callback

--- 11/001_Callbacks/main.js
+++ 11/001_Callbacks/main.js
@@ -8,3 +8,16 @@ d3.selectAll('li')
     .style('font-size', () => {
         return Math.random() * 40 + 'px';
 });
+
+//--------------------
+// Callback structure:
+// function(_, idx) {
+//     code here
+// }
+//--------------------
+
+// Change background color based on index:
+d3.selectAll('li')
+    .style('background-color', (_, idx) => {
+        return idx % 2 === 0 ? 'lightgrey' : 'blue';
+    });
